a9b347c06214546f37b3ce3202ae0c371f6efba4
israncho <isragutierrez@ciencias.unam.mx>
2021-03-24 02:32:51 -0600

    the real first drawing steps

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,8 +3,11 @@ package app;
 import processing.core.PApplet;
 
 public class Main extends PApplet {
-    public static void main(String[] args) {
-        PApplet.main(Main.class.getName());
+
+    private Maze maze;
+
+    public Main(int mazeSize) {
+        this.maze = new Maze(mazeSize, 500);
     }
 
     @Override
@@ -22,10 +25,51 @@ public class Main extends PApplet {
 
     }
 
-    @Override
-    public void keyPressed(){
-        if (key == 'q'|| key == 'Q')
-            exit();
+    /**
+     * Method that draws the maze.
+     */
+    public void drawMaze() {
+        Box[][] grid = this.maze.getGrid();
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid.length; j++)
+                for (int k = 0; k < 4; k++)
+                    if (grid[i][j].getWall(k)) {
+                        Point[] points = grid[i][j].getWallPoints(k);
+                        line(points[0].getX(), points[0].getY(), points[1].getX(), points[1].getY());
+                    }
     }
 
+    @Override
+    public void keyPressed() {
+        if (key == 'q' || key == 'Q')
+            exit();
+        if (key == ENTER)
+            drawMaze();
+    }
+
+    public static void main(String[] args) {
+        String argMazeSize = "";
+        boolean canExecute = true;
+        int mazeSize = 0;
+        try {
+            argMazeSize = args[0];
+        } catch (Exception e) {
+            canExecute = false;
+            System.out.println("----Error in first argument----");
+            System.out.println(e.getMessage());
+        }
+        if (canExecute)
+            try {
+                mazeSize = Integer.parseInt(argMazeSize);
+            } catch (Exception e) {
+                canExecute = false;
+                System.out.println("----Error, the argument is not an integer----");
+                System.out.println(e.getMessage());
+            }
+        if (canExecute) {
+            Main myMain = new Main(mazeSize);
+            String[] sketchArgs = { "Maze" };
+            PApplet.runSketch(sketchArgs, myMain);
+        }
+    }
 }
